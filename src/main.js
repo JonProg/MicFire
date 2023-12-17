bbb10ba17da92a4456ce608e88aee6e4792a399e
jon_prog <jonashenri.aquino@gmail.com>
2023-12-16 23:25:05 -0300

    add main.js

---
+++ src/main.js
@@ -0,0 +1,29 @@
+navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(function(stream) {
+        const audioContext = new AudioContext();
+        const analyser = audioContext.createAnalyser();
+        const microphone = audioContext.createMediaStreamSource(stream);
+
+        microphone.connect(analyser);
+
+        // Configurando a análise de áudio
+        analyser.fftSize = 2048;
+        const bufferLength = analyser.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
+
+        // Função para processar os dados de áudio
+        function processAudio() {
+            analyser.getByteTimeDomainData(dataArray);
+
+            // Aqui você pode processar os dados para obter a frequência fundamental
+            // Por exemplo, você pode usar algoritmos de detecção de picos ou transformada de Fourier para calcular a frequência fundamental.
+
+            // Depois de obter a frequência fundamental, você pode usá-la para determinar a altura da voz.
+        }
+
+        // Chamando a função de processamento de áudio periodicamente
+        setInterval(processAudio, 100);
+    })
+    .catch(function(err) {
+        console.error('Erro ao acessar o microfone: ' + err);
+    });
